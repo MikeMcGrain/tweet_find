8b8874d6730fcb96ab854ae96b4a37fee1102b27
Mike McGrain <mcgrain.mike@gmail.com>
2020-04-06 17:07:43 -0500

    Search bar

--- src/Navigate.js
+++ src/Navigate.js
@@ -3,12 +3,12 @@ import { Navbar, Nav } from "react-bootstrap/"
 
 export default function Navigate() {
   return (
-    <Navbar className="navbar" expand="sm">
+    <Navbar bg="light" expand="sm">
       <Navbar.Brand href="/">TwitterLogo </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="nav-text">
-          <Nav.Link className="nav-text" href="/">Home</Nav.Link>
+        <Nav>
+          <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/search">Search</Nav.Link>
           <Nav.Link href="/recommend">Recommendations</Nav.Link>
         </Nav>
--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
+import InputGroup from "react-bootstrap/InputGroup"
+import Dropdown from "react-bootstrap/Dropdown"
+import DropdownButton from "react-bootstrap/DropdownButton"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
 
 export default function Search() {
+  const [hashOrAt, setHashOrAt] = useState("#")
   return (
     <div>
-      <h1>Search Page</h1>
+      <InputGroup className="mb-3">
+        <DropdownButton
+          as={InputGroup.Prepend}
+          variant="outline-secondary"
+          title={hashOrAt}
+          id="input-group-dropdown-1"
+        >
+          <Dropdown.Item onClick={() => setHashOrAt("#")}>#</Dropdown.Item>
+          <Dropdown.Item onClick={() => setHashOrAt("@")}>@</Dropdown.Item>
+        </DropdownButton>
+        <Form.Control aria-describedby="basic-addon1" />
+        <Button as={InputGroup.Append}>Search</Button>
+      </InputGroup>
     </div>
   )
 }
