562c5ee9ef0b4a8ff4e9d2500b8c7837b9402552
Mike McGrain <mcgrain.mike@gmail.com>
2020-04-07 15:54:28 -0500

    placeholder text color

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Search() {
   return (
     <div className="searchbox">
-      <input type="text" placeholder="Username or Topic" />
+      <input type="text" placeholder="enter a username or topic..." autoFocus />
       <br />
       <button>@</button>
       <button>#</button>
