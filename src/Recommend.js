2b6e4ab098d8de34603cc80963d10317257ef9d9
Mike McGrain <mcgrain.mike@gmail.com>
2020-04-08 22:35:47 -0500

    start Reccomend.js

--- src/Recommend.js
+++ src/Recommend.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
+import { Container, Col, Row } from "react-bootstrap"
+import Image from "react-bootstrap/Image"
 
 export default function Recommend() {
   return (
     <div>
       <h1>Recommended Twitterers</h1>
       <p>Same as search page, but with five fixed selections</p>
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image src="holder.js/171x180" rounded />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image src="holder.js/171x180" roundedCircle />
+          </Col>
+          <Col xs={6} md={4}>
+            <Image src="holder.js/171x180" thumbnail />
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
--- src/Search.js
+++ src/Search.js
@@ -3,49 +3,119 @@ import { Container, Col, Row } from "react-bootstrap"
 import Card from "react-bootstrap/Card"
 
 export default function Search() {
-  const [tweets, setTweets] = useState([
-    {
-      image: "user image",
-      name: "Bob",
-      screenName: "@BobsTwitter",
-      text: "blah blah blah",
-      date: "YYYY-MM-DD",
-    },
-    {
-      image: "user image",
-      name: "Horace Silver",
-      screenName: "@SilverTwitter",
-      text: "blah blah blah",
-      date: "YYYY-MM-DD",
-    },
-    {
-      image: "user image",
-      name: "Sonny Clark",
-      screenName: "@SonnysTwitter",
-      text: "blah blah blah",
-      date: "YYYY-MM-DD",
-    },
-    {
-      image: "user image",
-      name: "Cleavon Chesterton",
-      screenName: "@LevonCleavon",
-      text: "blah blah blah",
-      date: "YYYY-MM-DD",
-    },
-    {
-      image: "user image",
-      name: "Eustice Jackson",
-      screenName: "@JacksonTwitter",
-      text: "blah blah blah",
-      date: "YYYY-MM-DD",
-    },
-  ])
+  const [searchTerm, setSearchTerm] = useState("")
+  const [tweets, setTweets] = useState([])
+
+  function handleInput(e) {
+    setSearchTerm(e.target.value)
+  }
+
+  function searchByUser() {
+    setSearchTerm("")
+    setTweets([
+      {
+        image: "user image",
+        name: "Bob",
+        screenName: "@BobsTwitter",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+      {
+        image: "user image",
+        name: "Horace Silver",
+        screenName: "@SilverTwitter",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+      {
+        image: "user image",
+        name: "Sonny Clark",
+        screenName: "@SonnysTwitter",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+      {
+        image: "user image",
+        name: "Cleavon Chesterton",
+        screenName: "@LevonCleavon",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+      {
+        image: "user image",
+        name: "Eustice Jackson",
+        screenName: "@JacksonTwitter",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+    ])
+  }
+
+  function searchByTopic() {
+    setSearchTerm("")
+    setTweets([
+      {
+        image: "user image",
+        name: "Bill",
+        screenName: "@BillsTwitter",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+      {
+        image: "user image",
+        name: "Horace Silver",
+        screenName: "@SilverTwitter",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+      {
+        image: "user image",
+        name: "Sonny Clark",
+        screenName: "@SonnysTwitter",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+      {
+        image: "user image",
+        name: "Cleavon Chesterton",
+        screenName: "@LevonCleavon",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+      {
+        image: "user image",
+        name: "Eustice Jackson",
+        screenName: "@JacksonTwitter",
+        text: "blah blah blah",
+        date: "YYYY-MM-DD",
+      },
+    ])
+  }
+
+  const renderTweets = tweets.map((tweet) => {
+    return (
+      <Card className="tweet-card" key={tweet.screenName}>
+        <Card.Body>
+          <img src={tweet.image} />
+          <p>
+            {tweet.name}
+            {tweet.screenName}
+          </p>
+          <p>{tweet.text}</p>
+          <p>{tweet.date}</p>
+        </Card.Body>
+      </Card>
+    )
+  })
+
   return (
     <Container fluid>
       <Row>
         <Col xs={12} sm={5} className="text-center">
           <div className="searchbox">
             <input
+              onChange={handleInput}
+              value={searchTerm}
               type="text"
               placeholder="enter a username or topic..."
               autoFocus
@@ -53,32 +123,20 @@ export default function Search() {
             <Container fluid>
               <Row>
                 <Col>
-                  <button className="button-style">@</button>
+                  <button className="button-style" onClick={searchByUser}>
+                    @
+                  </button>
                 </Col>
                 <Col>
-                  <button className="button-style">#</button>
+                  <button className="button-style" onClick={searchByTopic}>
+                    #
+                  </button>
                 </Col>
               </Row>
             </Container>
           </div>
         </Col>
-        <Col>
-          <Card className="tweet-card">
-            <Card.Body>
-              <blockquote className="blockquote mb-0">
-                <p>
-                  {" "}
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer posuere erat a ante.{" "}
-                </p>
-                <footer className="blockquote-footer">
-                  Someone famous in{" "}
-                  <cite title="Source Title">Source Title</cite>
-                </footer>
-              </blockquote>
-            </Card.Body>
-          </Card>
-        </Col>
+        <Col>{renderTweets}</Col>
       </Row>
     </Container>
   )
