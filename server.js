1eed677f6d0ce541f53ac6b50830f9cbc120c54b
Mike McGrain <mcgrain.mike@gmail.com>
2020-04-13 12:04:24 -0500

    added route per page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import path from "path"
+import axios from "axios"
 
 const app = express()
 
@@ -7,10 +8,19 @@ const PORT = 3000
 
 app.use(express.static("client/build"))
 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-)
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(_dirname + "/client/build", "index.html"))
 })
+
+app.get("/search", (req, res) => {
+  res.sendFile(path.join(_dirname + "/client/build", "index.html"))
+})
+
+app.get("/recommend", (req, res) => {
+  res.sendFile(path.join(_dirname + "/client/build", "index.html"))
+})
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+)
